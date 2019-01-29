e9189536a45c834607663900c9e23b8aaf20d9f8
Alvaro Valarezo <lfvalare@espol.edu.ec>
2019-01-28 23:57:06 -0500

    Cambios en conexion

--- DSProyecto/src/Modelo/Conexion.java
+++ DSProyecto/src/Modelo/Conexion.java
@@ -7,11 +7,9 @@ package Modelo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.control.Alert;
